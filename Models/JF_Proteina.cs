130914ac0678de8fe145ad89bef811b7250051a6
NotGuatas <josephaugustoflores@gmail.com>
2024-10-28 09:33:46 -0500

    Creación de Proyecto

---
+++ Models/JF_Proteina.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JosephFlores_Examen1P.Models
+{
+    public class JF_Proteina
+    {
+        [Key]
+        public int ProteinaId { set; get; }
+
+        [Required(Erromessage)]
+    }
+}
